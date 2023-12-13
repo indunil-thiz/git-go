392fd51f43bbf42735d2dc13f661e48d0878aef9
jay <jay@gmail.com>
2023-12-13 22:11:50 +0530

    version 4 released!

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js file');
-console.log('version 3');
+console.log('version 4');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 console.log('Hello World');
-console.log('version 3');
-console.log('version 3.2 ');
+console.log('version 4');
+console.log('version 4');
