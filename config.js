73f1bc82831deec53e31123055854bf5581a004c
jay <jay@gmail.com>
2023-12-13 21:08:37 +0530

    ignore git file added to v3

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js file');
-console.log('version 2');
+console.log('version 3');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('Hello World');
-console.log('version 2');
+console.log('version 3');
